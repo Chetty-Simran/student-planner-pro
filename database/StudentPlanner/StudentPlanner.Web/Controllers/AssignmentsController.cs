bf78b416f2f0717063d13eeba9b1bf25204b92da
Simran Chetty <ST10471196@imconnect.edu.za>
2026-07-01 20:13:43 +0200

    Set up Student Planner Pro project structure and MVC application

---
+++ database/StudentPlanner/StudentPlanner.Web/Controllers/AssignmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPlanner.Web.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ database/StudentPlanner/StudentPlanner.Web/Models/Assignment.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentPlanner.Web.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Module { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
